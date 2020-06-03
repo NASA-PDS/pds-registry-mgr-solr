4cedc833dcb1c605b45b62fbb9b387bd8223a418
Eugene <karpenko@RAYL-AI846797.jpl.nasa.gov>
2020-06-03 15:48:33 -0700

    Started working on status update by package id

--- src/main/java/gov/nasa/pds/registry/mgr/cmd/SetArchiveStatusCmd.java
+++ src/main/java/gov/nasa/pds/registry/mgr/cmd/SetArchiveStatusCmd.java
@@ -11,6 +11,7 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrInputDocument;
 
 import gov.nasa.pds.registry.mgr.Constants;
@@ -21,10 +22,10 @@ public class SetArchiveStatusCmd implements CliCommand
 {
     private Set<String> statusNames; 
 
-    private String collectionName;
-    private String status;
-    private String lidvid;
-    private String packageId;
+    private String mCollectionName;
+    private String mStatus;
+    private String mLidvid;
+    private String mPackageId;
     
     
     public SetArchiveStatusCmd()
@@ -59,13 +60,13 @@ public class SetArchiveStatusCmd implements CliCommand
         }
 
         // Read and validate parameters
-        this.collectionName = cmdLine.getOptionValue("collection", Constants.DEFAULT_REGISTRY_COLLECTION);
+        mCollectionName = cmdLine.getOptionValue("collection", Constants.DEFAULT_REGISTRY_COLLECTION);
         if(!getStatus(cmdLine)) return;
         if(!getIds(cmdLine)) return;
 
         // Update status
         SolrClient client = SolrUtils.createSolrClient(cmdLine);
-        System.out.println("[INFO] Solr collection: " + collectionName);
+        System.out.println("[INFO] Solr collection: " + mCollectionName);
         updateStatus(client);
     }
 
@@ -81,8 +82,8 @@ public class SetArchiveStatusCmd implements CliCommand
             return false;
         }
 
-        this.status = tmp.toUpperCase();
-        if(!statusNames.contains(status))
+        mStatus = tmp.toUpperCase();
+        if(!statusNames.contains(mStatus))
         {
             System.out.println("[ERROR] Invalid '-status' parameter value: '" + tmp + "'");
             System.out.println();
@@ -96,10 +97,10 @@ public class SetArchiveStatusCmd implements CliCommand
     
     private boolean getIds(CommandLine cmdLine)
     {
-        this.lidvid = cmdLine.getOptionValue("lidvid");
-        this.packageId = cmdLine.getOptionValue("packageId");
+        mLidvid = cmdLine.getOptionValue("lidvid");
+        mPackageId = cmdLine.getOptionValue("packageId");
 
-        if(lidvid == null && packageId == null)
+        if(mLidvid == null && mPackageId == null)
         {
             System.out.println("[ERROR] Either '-lidvid' or '-packageId' parameter is required");
             System.out.println();
@@ -107,7 +108,7 @@ public class SetArchiveStatusCmd implements CliCommand
             return false;
         }
 
-        if(lidvid != null && packageId != null)
+        if(mLidvid != null && mPackageId != null)
         {
             System.out.println("[ERROR] Could not have both '-lidvid' and '-packageId' parameters");
             System.out.println();
@@ -123,11 +124,11 @@ public class SetArchiveStatusCmd implements CliCommand
     {
         try
         {
-            if(lidvid != null)
+            if(mLidvid != null)
             {
                 updateStatusByLidvid(client);
             }
-            else if(packageId != null)
+            else if(mPackageId != null)
             {
                 updateStatusByPackageId(client);
             }
@@ -141,41 +142,82 @@ public class SetArchiveStatusCmd implements CliCommand
     
     private void updateStatusByLidvid(SolrClient client) throws Exception
     {
-        System.out.println("[INFO] Updating arhive status. Lidvid = " + lidvid + ", status = " + status);
+        System.out.println("[INFO] Updating arhive status. Lidvid = " + mLidvid + ", status = " + mStatus);
         
         // Check that the lidvid exists        
-        SolrQuery solrQuery = new SolrQuery("lidvid:\"" + lidvid + "\"");
+        SolrQuery solrQuery = new SolrQuery("lidvid:\"" + mLidvid + "\"");
         solrQuery.set("fl", "lidvid");
 
         // Get Solr doc by lidvid
-        QueryResponse resp = client.query(collectionName, solrQuery);
+        QueryResponse resp = client.query(mCollectionName, solrQuery);
         long numDocs = resp.getResults().getNumFound();
         if(numDocs == 0)
         {
-            System.out.println("[ERROR] Could not find a document with lidvid = " + lidvid);
+            System.out.println("[ERROR] Could not find a document with lidvid = " + mLidvid);
             return;
         }
         
         // Update document
-        SolrInputDocument doc = new SolrInputDocument();
-        doc.addField("lidvid", lidvid);
-        
-        Map<String, String> map = new TreeMap<>();
-        map.put("set", status);
-        doc.addField("archive_status", map);
-
         List<SolrInputDocument> docs = new ArrayList<>();
+        SolrInputDocument doc = createUpdateDoc(mLidvid, createStatusMap(mStatus));
         docs.add(doc);
         
-        client.add(this.collectionName, docs);
-        client.commit(this.collectionName);
+        client.add(mCollectionName, docs);
+        client.commit(mCollectionName);
         System.out.println("[INFO] Done");
     }
     
-
-    private void updateStatusByPackageId(SolrClient client)
+    
+    private static Map<String, String> createStatusMap(String status)
     {
+        Map<String, String> map = new TreeMap<>();
+        map.put("set", status);
+        return map;
+    }
+    
+    
+    private static SolrInputDocument createUpdateDoc(String lidvid, Map<String, String> statusMap)
+    {
+        SolrInputDocument doc = new SolrInputDocument();
+        doc.addField("lidvid", lidvid);
+        doc.addField("archive_status", statusMap);
+        return doc;
+    }
+    
+
+    private void updateStatusByPackageId(SolrClient client) throws Exception
+    {
+        System.out.println("[INFO] Updating arhive status. PackageId = " + mPackageId + ", status = " + mStatus);
         
+        Map<String, String> statusMap = createStatusMap(mStatus);
+        
+        SolrQuery solrQuery = new SolrQuery("_package_id:\"" + mPackageId + "\"");
+        solrQuery.set("fl", "lidvid");
+
+        // Get Solr docs by packageId
+        QueryResponse resp = client.query(mCollectionName, solrQuery);
+        long numDocs = resp.getResults().getNumFound();
+        if(numDocs == 0)
+        {
+            System.out.println("[ERROR] There are no documents with packageId = " + mPackageId);
+            return;
+        }
+
+        // Create update batch
+        List<SolrInputDocument> batch = new ArrayList<>();
+        
+        for(SolrDocument respDoc: resp.getResults())
+        {
+            String lidvid = (String)respDoc.getFirstValue("lidvid");
+            SolrInputDocument updateDoc = createUpdateDoc(lidvid, statusMap);
+            batch.add(updateDoc);
+        }
+
+        // Add and commit the batch
+        client.add(mCollectionName, batch);
+        client.commit(mCollectionName);
+        
+        System.out.println("[INFO] Done");
     }
     
     
